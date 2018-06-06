a17dba859f76da5fbf527fc6fa8d12df18346db4
Joshua Rippon <joshua@pomerolpartners.com>
2018-06-06 14:13:05 +0100

    Using pug

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("env2")(".env");
 const nodemailer = require("nodemailer");
-
+const Email = require("email-templates");
 const transporter = nodemailer.createTransport({
     service: process.env.SERVICE,
     auth: {
@@ -9,10 +9,34 @@ const transporter = nodemailer.createTransport({
     }
 });
 
-transporter.sendMail({
-    from: process.env.USER,
-    to: "joshuarippon1@gmail.com",
-    subject: "HEY MAN",
-    text: "WASSUP DUDE",
-    html: "<h1>OI OI OI OI</h1>"
+const email = new Email({
+    message: {
+        from: process.env.USER
+    },
+    transport: transporter,
+    views: {
+        options: {
+            extension: "hbs"
+        }
+    }
 });
+
+email
+    .send({
+        template: "mars",
+        message: {
+            to: "joshuarippon1@gmail.com"
+        },
+        locals: {
+            names: ["Josh"]
+        }
+    })
+    .then(console.log)
+    .catch(console.error);
+// transporter.sendMail({
+//     from: process.env.USER,
+//     to: "joshuarippon1@gmail.com",
+//     subject: "HEY MAN",
+//     text: "WASSUP DUDE",
+//     html: "<h1>OI OI OI OI</h1>"
+// });
